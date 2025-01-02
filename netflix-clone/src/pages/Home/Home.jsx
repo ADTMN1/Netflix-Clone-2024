480b31ea47934746316b6aa88ae948282dd94622
ADTMN1 <163424703+ADTMN1@users.noreply.github.com>
2025-01-02 09:42:22 -0800

    Banner

--- netflix-clone/src/pages/Home/Home.jsx
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
+import Banner from '../../components/Banner/Banner';
 import Footer from '../../components/Footer/Footer';
 const Home = () => {
-    return (
-        <div>
-          <Header/>
-          <Footer/>
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <Banner />
+      <Footer />
+    </div>
+  );
 }
 
 export default Home;
